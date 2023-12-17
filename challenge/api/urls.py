be68af71f1efc3b79136a36ac41f8933e8470658
guyacamole <guy.xd2000@gmail.com>
2023-12-16 20:55:06 -0600

    read without token

--- challenge/api/urls.py
+++ challenge/api/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 from api import views
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import AllowAny
 from .views import PropertyViewSet
-
 
 router = routers.DefaultRouter()
 router.register(r'properties', views.PropertyViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', api_view(['GET'])(authentication_classes([])(permission_classes([AllowAny])(PropertyViewSet))))
 ]
--- challenge/api/views.py
+++ challenge/api/views.py
@@ -2,8 +2,14 @@ from rest_framework import viewsets
 from .serializer import PropertySerializer
 from .models import Property
 from rest_framework import authentication, permissions
+
 class PropertyViewSet(viewsets.ModelViewSet):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            self.permission_classes = [permissions.AllowAny]
+        return super(PropertyViewSet, self).get_permissions()
